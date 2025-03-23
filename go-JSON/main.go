8d024c0738bd844f6b210b5289c7b5d0d93d7860
Lynxprazo <issamwanga02@gmail.com>
2025-03-23 11:21:56 +0300

    go struct to json

--- go-JSON/main.go
+++ go-JSON/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
 
+)
+
+// create user struct
+type User struct{
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Age int `json:"age"`
+}
 func main() {
 fmt.Println("learing golang/json")
+
+// start convert Go struct into the Json 
+user := User{Name: "Issa", Email: "prazoo45@gmail.com", Age:23}
+if jsonData,err := json.Marshal(user);err != nil{
+	fmt.Errorf("faild to marshail %v",err)
+	return
+}else{
+	fmt.Println(string(jsonData))
+}
+
 }
