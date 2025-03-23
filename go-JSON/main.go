49bc2c219147ec5c7526574281410aa214946a5b
Lynxprazo <issamwanga02@gmail.com>
2025-03-23 15:00:06 +0300

    more changes

--- go-JSON/main.go
+++ go-JSON/main.go
@@ -16,6 +16,8 @@ func main() {
 fmt.Println("learing golang/json")
 StructTJson()
 JsonTstruct()
+MapTjson()
+JsonTMap()
 
 }
 // start convert Go struct into the Json 
@@ -43,4 +45,34 @@ func JsonTstruct()(string, error){
 	result := fmt.Sprintf("Name: %s, Email: %s, Age: %d", details.Name, details.Email, details.Age)
 	fmt.Println(result)
    return result, nil
+}
+// convert map go into json
+
+func MapTjson()(string, error){
+	data := map[string]interface{}{
+		"Age":23,
+		"Email":"Prazzo45@gmail.com",
+		"Name":"Issa",
+
+	}
+	jsonData, err := json.Marshal(data)
+	if err != nil{
+		return "",fmt.Errorf("Failed to Marshal Map",err)
+	}
+	result := string(jsonData)
+	fmt.Println(result)
+	return result, nil
+}
+// convert json to Map go
+
+func JsonTMap()(string, error){
+var data map[string] interface{}
+user := `{"name":"Issa", "email":"Prazoo45@gmail.com", "age":23}`
+err := json.Unmarshal([]byte(user), &data)
+if err != nil{
+	return "", fmt.Errorf("Failed to Decode the user",err)
+}
+result := fmt.Sprintf("name is %s email is %s and age is %d",data["name"], data["email"], data["age"])
+fmt.Println(result)
+return result, nil
 }
